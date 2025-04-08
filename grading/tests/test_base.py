236c90ae43026ab625e9b74d1e16b44fe4a28a01
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-08 20:10:38 -0300

    Modifications on test_base.py and feedback.json.

--- grading/tests/test_base.py
+++ grading/tests/test_base.py
@@ -1,14 +1,16 @@
 import os
 from bs4 import BeautifulSoup
 import re
+from utils.find_file import find_file_by_extension
 # === Helpers ===
-def load_html(file_path="index.html"):
+def load_html():
     """
     Loads and parses the HTML file using BeautifulSoup.
     Returns (soup, None) if successful, or (None, error_message) if file not found.
     """
-    if not os.path.exists(file_path):
-        return None, "❌ File 'index.html' not found"
+    file_path = find_file_by_extension('.html')
+    if not file_path:
+        return None,"❌ File 'index.html' not found"
     with open(file_path, "r", encoding="utf-8") as file:
         return BeautifulSoup(file.read(), "html.parser"), None
 
