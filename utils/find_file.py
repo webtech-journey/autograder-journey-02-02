3234519867067932d811cdfb81dd5a97aa35235a
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-08 20:59:48 -0300

    Adding debugging steps

--- utils/find_file.py
+++ utils/find_file.py
@@ -1,11 +1,14 @@
 import os
 
-
 def find_file_by_extension(extension):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+def find_file_by_extension_up(extension):
     # Get the current directory, then go up two levels
     current_directory = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.abspath(os.path.join(current_directory, '..'))  # Go up two levels
-    print(parent_directory)
+    print("Current dir: " + current_directory)
+    print("Parent dir: "+parent_directory)
     # Loop through all files in the parent directory
     for filename in os.listdir(parent_directory):
         if filename.endswith(extension):
