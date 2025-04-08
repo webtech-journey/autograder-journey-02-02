89aad5c39aab7fabc7ac5b64a6b90b977689a1e9
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-08 20:44:23 -0300

    Adding debugging step

--- utils/report_generator.py
+++ utils/report_generator.py
@@ -25,6 +25,7 @@ def generate_md(base, bonus, penalty,final_score,author,feedback_file="feedback.
     # Load feedback data from the JSON file
     with open(path.getFilePath(feedback_file), "r", encoding="utf-8") as file:
         tests_feedback = json.load(file)
+    print(tests_feedback["base_tests"])
     passed = True if final_score >= 70 else False
     # Initialize feedback
     feedback = f"# 🧪 Relatório de Avaliação – Autograder HTML - {author}\n\n"
