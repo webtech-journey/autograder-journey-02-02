7bfd4121a8ad8c3c38767d0c785ad278fe9308cd
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-09 07:28:19 -0300

    Fixing final issues

--- utils/commit_report.py
+++ utils/commit_report.py
@@ -1,12 +1,12 @@
 from github import Github
 import base64
 import os
-def overwrite_report_in_repo(file_path="relatorio.md", new_content=""):
+def overwrite_report_in_repo(token,file_path="relatorio.md", new_content=""):
     try:
         # Get the current file content (if it exists)
         repo = os.getenv("GITHUB_REPOSITORY")
-        org = repo.split('/')[0]
-        repo_name = repo.split('/')[1]
+        g = Github(token)
+        repo = g.get_repo(repo)
         try:
             file = repo.get_contents(file_path)
             file_sha = file.sha
