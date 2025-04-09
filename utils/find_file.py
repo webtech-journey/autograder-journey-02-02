11035b0d7ab18b20689d0dd42990ff73c1ec6d5d
ArthurSGA <arodrigues@sga.pucminas.br>
2025-04-08 21:24:14 -0300

    Fix attempt on file path

--- utils/find_file.py
+++ utils/find_file.py
@@ -1,12 +1,16 @@
 import os
 
-def find_file_by_extension(extension):
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+def find_file_by_extension(file,extension):
+    current_directory = os.path.dirname(os.path.abspath(file))
+    for filename in os.listdir(current_directory):
+        if filename.endswith(extension):
+            return filename
+    return None
 
 def find_file_by_extension_up(extension):
     # Get the current directory, then go up two levels
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    parent_directory = os.path.abspath(os.path.join(current_directory, '..'))  # Go up two levels
+    parent_directory = os.path.abspath(os.path.join(current_directory, '.'))  # Go up two levels
     print("Current dir: " + current_directory)
     print("Parent dir: "+parent_directory)
     # Loop through all files in the parent directory
